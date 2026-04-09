7f1d36ff0123571dc39a20b864ccc8ee7b32e3d9
JamesAtkins444 <52932608+JamesAtkins444@users.noreply.github.com>
2026-04-09 14:51:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -75,9 +75,6 @@ app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+
 
 
